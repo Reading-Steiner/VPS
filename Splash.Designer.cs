620b1bad6b317a14425e6d369d140824fdcbf2bb
“Reading-Steiner <483665176@qq.com>
2020-08-24 09:22:48 +0800

    启动界面防闪烁

--- Splash.Designer.cs
+++ Splash.Designer.cs
@@ -77,6 +77,7 @@
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.TXT_version);
             this.Controls.Add(this.label1);
+            this.DoubleBuffered = true;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
